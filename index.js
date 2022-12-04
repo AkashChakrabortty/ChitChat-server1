f3061edf990c8314ddbc85c1d73008b25104a69e
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-12-05 00:07:46 +0600

    friend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const {MongoClient, ServerApiVersion} =  require('mongodb');
+const {MongoClient, ServerApiVersion, ObjectId} =  require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -18,6 +18,9 @@ async function run(){
      const userCollection = client.db('chitchat').collection('user');
      const postCollection = client.db('chitchat').collection('post');
      const storyCollection = client.db('chitchat').collection('story');
+     const requestCollection = client.db('chitchat').collection('request');
+
+     
      //insert every new user
      app.post('/create', async(req,res)=> {
         const user = req.body;
@@ -37,6 +40,22 @@ async function run(){
         const result = await storyCollection.insertOne(story);
         res.send(result);
        })
+
+       //insert user's friend req info
+      app.post('/addFriend', async(req,res)=>{
+         const reqInfo = req.body;
+         const query = {receiver_email:reqInfo.receiver_email, sender_email:reqInfo.sender_email};
+         const result = await requestCollection.findOne(query)
+        
+         if(result){
+           res.send({acknowledged:false})
+         }
+         else{
+           const result1 = await requestCollection.insertOne(reqInfo);
+           res.send(result1);
+         }
+      
+       })
      //get user name
      app.get('/user/:email', async(req,res)=>{
       const email = req.params.email;
@@ -44,6 +63,13 @@ async function run(){
       res.send(result);
      })
      
+      //get all users
+      app.get('/peoples', async(req,res)=>{
+        const query = {}
+        const cursor = userCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+       })
       //get specific user's post
       app.get('/post/:email', async(req,res)=>{
         const email = req.params.email;
@@ -52,15 +78,30 @@ async function run(){
         const result = await cursor.toArray();
         res.send(result);
        })
-
-        //get specific user's story
-      app.get('/story/:email', async(req,res)=>{
+       //get specific user's friend request
+       app.get('/request/:email',async(req,res)=> {
         const email = req.params.email;
-        const query = {user_email: email}
-        const cursor = storyCollection.find(query).sort( { "milliseconds": -1 } );
+        const query = {receiver_email: email};
+        const cursor = requestCollection.find(query).sort( { "milliseconds": -1 } );
         const result = await cursor.toArray();
         res.send(result);
        })
+
+       //delete friend request
+       app.delete('/reqDelete/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = requestCollection.deleteOne(query);
+        res.send(result);
+       })
+      //   //get specific user's story
+      // app.get('/story/:email', async(req,res)=>{
+      //   const email = req.params.email;
+      //   const query = {user_email: email}
+      //   const cursor = storyCollection.find(query).sort( { "milliseconds": -1 } );
+      //   const result = await cursor.toArray();
+      //   res.send(result);
+      //  })
   }
   catch{
     (err => console.log(err) )
